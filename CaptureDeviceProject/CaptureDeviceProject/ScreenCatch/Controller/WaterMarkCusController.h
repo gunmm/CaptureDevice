5e21960aab1db28a7a384afd7728852ee8d6e1f2
minzhe <924744097@qq.com>
2020-01-16 21:26:13 +0800

    🚧 文件共享

---
+++ CaptureDeviceProject/CaptureDeviceProject/ScreenCatch/Controller/WaterMarkCusController.h
@@ -0,0 +1,17 @@
+//
+//  WaterMarkCusController.h
+//  CaptureDeviceProject
+//
+//  Created by gunmm on 2020/1/16.
+//  Copyright © 2020 minzhe. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WaterMarkCusController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CaptureDeviceProject/CaptureDeviceProject/ScreenCatch/Controller/WaterMarkCusController.m
@@ -0,0 +1,28 @@
+//
+//  WaterMarkCusController.m
+//  CaptureDeviceProject
+//
+//  Created by gunmm on 2020/1/16.
+//  Copyright © 2020 minzhe. All rights reserved.
+//
+
+#import "WaterMarkCusController.h"
+
+@interface WaterMarkCusController ()
+
+@property (weak, nonatomic) IBOutlet UIView *topView;
+
+@end
+
+@implementation WaterMarkCusController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)addBtnAct:(id)sender {
+    
+}
+
+@end
